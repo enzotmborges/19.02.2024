b6a2e135c4da16ff94e451ea4d15e83229ca4a82
Enzo Takashi Martinatto Borges <112429924+enzotmborges@users.noreply.github.com>
2024-02-19 17:14:42 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import {exibirHorario} from './components/horario';
+import {exibirHorario} from './components/Horario';
+import {fazerLogin} from './components/Login';
 
 function App() {
   return (
@@ -15,9 +16,22 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          id="lnk"
         >
           Learning React
         </a>
+        <br />
+        <input type="text" id="txtLogin" />
+        <br />
+        <input type="text" id="txtSenha" />
+        <br />
+        <button
+          onClick={fazerLogin}
+        >Fazer Login</button>
+        <br />
+        <button
+          onClick={exibirHorario}
+        >Ver Horário</button>
       </header>
     </div>
   );
